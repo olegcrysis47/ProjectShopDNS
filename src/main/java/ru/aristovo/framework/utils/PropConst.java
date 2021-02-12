21c2ea1ebe8c9062158ef9308999c649cea21b0d
Oleg Pushkarev <tyler98765@yandex.ru>
2021-02-13 01:53:14 +0300

    Создан класс с константами для файла properties

---
+++ src/main/java/ru/aristovo/framework/utils/PropConst.java
@@ -0,0 +1,16 @@
+package ru.aristovo.framework.utils;
+
+// класс констант из файла properties
+public class PropConst {
+
+    public static final String APP_URL = "app.url";
+
+    public static final String TYPE_BROWSER = "type.browser";
+
+    public static final String PATH_CHROME_DRIVER_WINDOWS = "path.chrome.driver.windows";
+
+    public static final String IMPLICITLY_WAIT = "implicitly.wait";
+
+    public static final String PAGE_LOAD_TIMEOUT = "page.load.timeout";
+
+}
