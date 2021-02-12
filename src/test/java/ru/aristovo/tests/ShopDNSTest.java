14372358effe07814e56433c017471606e8eb88d
Oleg Pushkarev <tyler98765@yandex.ru>
2021-02-12 14:55:45 +0300

    Выполнено: 2. в поиске найти playstation 3. кликнуть по playstation 4 slim black

--- src/test/java/ru/aristovo/tests/ShopDNSTest.java
+++ src/test/java/ru/aristovo/tests/ShopDNSTest.java
@@ -1,8 +1,16 @@
 package ru.aristovo.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import ru.aristovo.base.BaseTests;
+
+import java.util.List;
 
 @DisplayName("Тестируем магазин DNS")
 public class ShopDNSTest extends BaseTests {
@@ -11,9 +19,33 @@ public class ShopDNSTest extends BaseTests {
     @DisplayName("Тест покупки PlayStation и пары дисков с играми")
     void buyPlayStationAndGames() {
 
+        // 2. в поиске найти playstation
+        String fieldFoundOnSiteXPath =
+                "//input[@class='ui-input-search__input " +
+                        "ui-input-search__input_presearch' and @placeholder='Поиск по сайту']";
+
+        WebElement fieldFoundOnSite = driver.findElement(By.xpath(fieldFoundOnSiteXPath));
+        waitUtilElementToBeClickable(fieldFoundOnSite);
+        fieldFoundOnSite.click();
+        fieldFoundOnSite.sendKeys("playstation");
+        Assertions.assertEquals("playstation", fieldFoundOnSite.getAttribute("value"),
+                "Поле было заполнено не верно");
+        fieldFoundOnSite.sendKeys(Keys.ENTER);
+
+        // 3. кликнуть по playstation 4 slim black
+        String selectAvailableProductXPath = "//div[@data-id='product']";
+        List<WebElement> selectAvailableProduct = driver.findElements(By.xpath(selectAvailableProductXPath));
+        for (WebElement w:selectAvailableProduct) {
+            WebElement p = w.findElement(By.xpath(".//a[@class='ui-link']"));
+            if (p.getText().toLowerCase().contains("playstation 4 slim black")) {
+                waitUtilElementToBeClickable(p);
+                p.click();
+                break;
+            }
+        }
+
+        waitThread(5000);
         /*
-        2. в поиске найти playstation
-        3. кликнуть по playstation 4 slim black
         4. запомнить цену
         5. Доп.гарантия - выбрать 2 года
         6. дождаться изменения цены и запомнить цену с гарантией
@@ -30,5 +62,30 @@ public class ShopDNSTest extends BaseTests {
         17. добавить еще 2 playstation (кнопкой +) и проверить что сумма верна (равна трем ценам playstation)
         18. нажать вернуть удаленный товар, проверить что Detroit появился в корзине и сумма увеличилась на его значение
          */
+    }
+
+    void waitThread(int milliSeconds) {
+        try {
+            Thread.sleep(milliSeconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void scrollToElementJs(WebElement element) {
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    void waitUtilElementToBeClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    void waitUtilElementToBeVisible(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    void waitUtilElementToBeVisible(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 }
