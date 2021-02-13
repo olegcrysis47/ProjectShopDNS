fe76647aa12573f4579fa52d0a1a2470575136c3
Oleg Pushkarev <tyler98765@yandex.ru>
2021-02-13 03:26:24 +0300

    Заготовлен класс BasePage, но от него пока не получается наследоваться.

--- src/main/java/ru/aristovo/framework/pages/BasePage.java
+++ src/main/java/ru/aristovo/framework/pages/BasePage.java
@@ -1,10 +1,31 @@
 package ru.aristovo.framework.pages;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
+/**
+ * Базовый класс, который содержит общие методы для всех классов страниц сайта.
+ */
 public class BasePage {
 
+    /**
+     * Конструктор.
+     * @param driver - веб-драйвер в который передается стартовая страница.
+     */
+    public BasePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 
+    /**
+     * Метод притормаживает тест, чтобы драйвер успел обновить данные на странице.
+     * @param milliSeconds - задаем время ожидания в миллисекундах.
+     */
+    public void waitThread(int milliSeconds) {
+        try {
+            Thread.sleep(milliSeconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
